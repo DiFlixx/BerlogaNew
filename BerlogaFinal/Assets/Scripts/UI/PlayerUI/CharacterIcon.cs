c80c66c179d5e90c7171059f21f78063c680eb11
RoilesGame <roilesgame0799@gmail.com>
2024-03-08 20:23:22 +0300

    дописал полностью обучение

---
+++ BerlogaFinal/Assets/Scripts/UI/PlayerUI/CharacterIcon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterIcon : MonoBehaviour
+{
+    private GameObject _icon;
+    private GameObject _player;
+    
+    void Update()
+    {
+        
+    }
+}
