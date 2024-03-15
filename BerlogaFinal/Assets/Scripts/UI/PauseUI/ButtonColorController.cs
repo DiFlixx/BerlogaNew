e7c3d875edda8d8189593ac86df9a3b138116637
RoilesGame <roilesgame0799@gmail.com>
2024-03-15 21:36:25 +0300

    Клуша теперь в игре!!!!

--- BerlogaFinal/Assets/Scripts/UI/PauseUI/ButtonColorController.cs
+++ BerlogaFinal/Assets/Scripts/UI/PauseUI/ButtonColorController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonColorController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Button[] headButtons;
+
+    private Button _menuButton;
+    private Button _settingsButton;
+    private Button _guideButton;
+    private Button _mapButton;
+
     void Start()
     {
-        
+        _menuButton = headButtons[0].GetComponent<Button>();
+        _settingsButton = headButtons[1].GetComponent<Button>();
+        _guideButton = headButtons[2].GetComponent<Button>();
+        _mapButton = headButtons[3].GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private int MakeNormalColor()
     {
-        
+        return 0;
     }
 }
