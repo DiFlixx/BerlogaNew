95d21e52017fadcd22b08c8cb914642067b6ac0e
RoilesGame <roilesgame0799@gmail.com>
2024-03-20 10:13:49 +0300

    Пофикшен баг с кнопками паузы

--- BerlogaFinal/Assets/Scripts/UI/PauseUI/ColorChanger.cs
+++ BerlogaFinal/Assets/Scripts/UI/PauseUI/ColorChanger.cs
@@ -3,15 +3,24 @@ using UnityEngine.UI;
 
 public class ColorChanger : MonoBehaviour
 {
-    private ButtonColorController _buttonColorController;
-
-    [SerializeField] private Button[] _headButtons;
+    [SerializeField] private Button[] headButtons;
+    private Button _button;
+    private ColorBlock _theColor;
 
     private void NormalizeAllButtons(Button[] buttons)
     {
-        foreach (var button in buttons)
+        for (int index = 0; index < buttons.Length; index++)
         {
-            _buttonColorController.MakeHeadButtonIsUsual();
+            _button = headButtons[index].GetComponent<Button>();
+            _theColor = _button.GetComponent<Button>().colors;
+            
+            _theColor.normalColor = new Color32(93, 15, 95, 0);
+            _button.colors = _theColor;
         }
+    }
+
+    public void MakeAllButtonsIsUsual()
+    {
+        NormalizeAllButtons(headButtons);
     }
 }
